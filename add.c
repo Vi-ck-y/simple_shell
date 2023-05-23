8224eea63d05fcd022b7c4df8b49f9a32b0632f6
Victoria <victoriaometieoluchi@gmail.com>
2023-05-23 13:30:38 -0700

    add.c

---
+++ add.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+/**
+ * add - Adds two int numbers
+ * @nos1: The first number to be added
+ * @nos2: The second number to be added
+ *
+ * Return: The result of the addition of nos1 and nos2
+ */
+
+int add(int nos1, int nos2)
+{
+	return (nos1 + nos2);
+}
---
+++ shell.h
@@ -0,0 +1,6 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+int add(int nos1, int nos2);
+
+#endif
